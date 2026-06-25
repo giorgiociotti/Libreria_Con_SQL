4d774b54be4eb262e9b414c024cbf0c81375cefe
giorgiociotti <gciotti29@gmail.com>
2026-06-25 12:21:06 +0200

    Refactor Database class: clean up comments and standardize schema name

--- src/com/generation/database/Database.java
+++ src/com/generation/database/Database.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//responsabilità: creare la connessione con il server mysql e chiuderla
 public class Database {
 
     private boolean connected;
@@ -24,20 +23,7 @@ public class Database {
         this.connected = false;
         this.username = "root";
         this.password = "root";
-        this.nomeSchema = "libreria154";
-
-        // indirizzo al server mysql, alla sua porta e allo specifico schema a cui
-        // connettersi
-        // jdbc:mysql è il protocollo che indica a jdbc quale driver servirà per
-        // relazionarsi con
-        // il DBMS, nel nostro caso è mysql per cui quando si passa questo indirizzo
-        // JDBC
-        // cerca automaticamente tra i driver importati con il jar quello specifico
-        // indicato qui
-        // e lo carica a RUNTIME fornendo gli strumenti necessari per collegarsi con il
-        // DBMS corretto
-        // se avessimo usato un DBMS diverso,come ad esempio postgres SQL allora:
-        // jdbc:postgresql
+        this.nomeSchema = "libreria";
         this.url = "jdbc:mysql://localhost:3306/" + nomeSchema + "?useSSL=false&serverTimezone=UTC";
     }
 
@@ -48,20 +34,14 @@ public class Database {
         return instance;
     }
 
-    // metodo che apre la connessione
     public void openConnection() {
 
         try {
             if (connection == null || connection.isClosed()) {
-                // prepara il driver necessario
-                // la riga 51 non è necessaria ma permette di capire il flosso delle azioni:
-                // PRIMA viene registratoil driver corretto
+                
                 Class.forName(DRIVER);
 
-                // poi si apre la connessione
-                // la classe DriverManager formisce al progrmma il driver corretto da utilzzare
-                // per lo specifico DBMS in uso, inoltre crea la connessione e permette
-                // di usare quindi i suoi metodi
+                
                 connection = DriverManager.getConnection(url, username, password);
                 connected = true;
             }
@@ -74,7 +54,6 @@ public class Database {
         }
     }
 
-    // metodo che chiude la connessione
     public void closeConnection() {
         try {
             if (connection != null && !connection.isClosed()) {
