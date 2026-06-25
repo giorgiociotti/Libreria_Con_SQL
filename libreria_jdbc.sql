1434084546873a317667d2b12b446d7c6a582d1c
giorgiociotti <gciotti29@gmail.com>
2026-06-25 10:27:48 +0200

    Update binary files and refactor SQL and Java code for improved formatting and clarity

--- libreria_jdbc.sql
+++ libreria_jdbc.sql
@@ -2,27 +2,27 @@ CREATE DATABASE IF NOT EXISTS libreria;
 
 use libreria;
 
-create table autori(
-	id int unsigned primary key auto_increment,
+create table autori (
+    id int unsigned primary key auto_increment,
     nome varchar(100),
     cognome varchar(100)
 );
 
-
-insert into autori(nome, cognome) values
-('Umberto', 'Eco'),
-('George', 'Orwell'),
-('F. Scott', 'Fitzgerald'),
-('Herman', 'Melville'),
-('Italo', 'Svevo'),
-('Anna', 'Frank'),
-('Miguel', 'de Cervantes'),
-('Jane', 'Austen'),
-('Alessandro', 'Manzoni'),
-('Antoine', 'de Saint-Exupéry'),
-('Antoine', 'Orwell'),
-('Antoine', 'Orwell'),
-('Antoine', 'Orwell');
+insert into
+    autori (nome, cognome)
+values ('Umberto', 'Eco'),
+    ('George', 'Orwell'),
+    ('F. Scott', 'Fitzgerald'),
+    ('Herman', 'Melville'),
+    ('Italo', 'Svevo'),
+    ('Anna', 'Frank'),
+    ('Miguel', 'de Cervantes'),
+    ('Jane', 'Austen'),
+    ('Alessandro', 'Manzoni'),
+    ('Antoine', 'de Saint-Exupéry'),
+    ('Antoine', 'Orwell'),
+    ('Antoine', 'Orwell'),
+    ('Antoine', 'Orwell');
 
 DROP TABLE IF EXISTS libri;
 
@@ -32,18 +32,69 @@ CREATE TABLE libri (
     autore INT UNSIGNED,
     anno_pubblicazione INT,
     prezzo DOUBLE,
-    FOREIGN KEY(autore) REFERENCES autori(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (autore) REFERENCES autori (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO libri (titolo, autore, anno_pubblicazione, prezzo) VALUES 
-('Il nome della rosa', 1, 1980, 40.00),
-('1984', 2, 1949, 18.00),
-('La fattoria degli animali', 2, 1937, 18.00),
-('Il grande Gatsby', 3, 1925, 10.00),
-('Moby Dick', 4, 1851, 9.99),
-('La coscienza di Zeno', 5, 1923, 13.75),
-('Il piccolo principe', 10, 1943, 8.99),
-('Il diario di Anna Frank', 6, 1947, 11.50),
-('Don Chisciotte della Mancia', 7, 1605, 14.99),
-('Orgoglio e pregiudizio', 8, 1813, 12.00),
-('I Promessi Sposi', 9, 1827, 18.00);
+INSERT INTO
+    libri (
+        titolo,
+        autore,
+        anno_pubblicazione,
+        prezzo
+    )
+VALUES (
+        'Il nome della rosa',
+        1,
+        1980,
+        40.00
+    ),
+    ('1984', 2, 1949, 18.00),
+    (
+        'La fattoria degli animali',
+        2,
+        1937,
+        18.00
+    ),
+    (
+        'Il grande Gatsby',
+        3,
+        1925,
+        10.00
+    ),
+    ('Moby Dick', 4, 1851, 9.99),
+    (
+        'La coscienza di Zeno',
+        5,
+        1923,
+        13.75
+    ),
+    (
+        'Il piccolo principe',
+        10,
+        1943,
+        8.99
+    ),
+    (
+        'Il diario di Anna Frank',
+        6,
+        1947,
+        11.50
+    ),
+    (
+        'Don Chisciotte della Mancia',
+        7,
+        1605,
+        14.99
+    ),
+    (
+        'Orgoglio e pregiudizio',
+        8,
+        1813,
+        12.00
+    ),
+    (
+        'I Promessi Sposi',
+        9,
+        1827,
+        18.00
+    );
--- src/com/generation/database/Database.java
+++ src/com/generation/database/Database.java
@@ -17,64 +17,67 @@ public class Database {
     //
     private Connection connection;
 
-    //percorso per raggiungere la classe Driver nella libreria
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; 
+    // percorso per raggiungere la classe Driver nella libreria
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    private Database(){
+    private Database() {
         this.connected = false;
         this.username = "root";
         this.password = "root";
         this.nomeSchema = "libreria154";
 
-        //indirizzo al server mysql, alla sua porta e allo specifico schema a cui connettersi
-        //jdbc:mysql è il protocollo che indica a jdbc quale driver servirà per relazionarsi con 
-        //il DBMS, nel nostro caso è mysql per cui quando si passa questo indirizzo JDBC 
-        //cerca automaticamente tra i driver importati con il jar quello specifico indicato qui
-        //e lo carica a RUNTIME fornendo gli strumenti necessari per collegarsi con il DBMS corretto
-        //se avessimo usato un DBMS diverso,come ad esempio postgres SQL allora:
-        //jdbc:postgresql
+        // indirizzo al server mysql, alla sua porta e allo specifico schema a cui
+        // connettersi
+        // jdbc:mysql è il protocollo che indica a jdbc quale driver servirà per
+        // relazionarsi con
+        // il DBMS, nel nostro caso è mysql per cui quando si passa questo indirizzo
+        // JDBC
+        // cerca automaticamente tra i driver importati con il jar quello specifico
+        // indicato qui
+        // e lo carica a RUNTIME fornendo gli strumenti necessari per collegarsi con il
+        // DBMS corretto
+        // se avessimo usato un DBMS diverso,come ad esempio postgres SQL allora:
+        // jdbc:postgresql
         this.url = "jdbc:mysql://localhost:3306/" + nomeSchema + "?useSSL=false&serverTimezone=UTC";
     }
 
-    public static Database getInstance(){
-        if(instance == null){
+    public static Database getInstance() {
+        if (instance == null) {
             instance = new Database();
         }
         return instance;
     }
 
-    //metodo che apre la connessione
-    public void openConnection(){
+    // metodo che apre la connessione
+    public void openConnection() {
 
-        try{
-            if(connection == null || connection.isClosed()){
-                //prepara il driver necessario
+        try {
+            if (connection == null || connection.isClosed()) {
+                // prepara il driver necessario
                 // la riga 51 non è necessaria ma permette di capire il flosso delle azioni:
-                //PRIMA viene registratoil driver corretto 
+                // PRIMA viene registratoil driver corretto
                 Class.forName(DRIVER);
 
-                //poi si apre la connessione
-                //la classe DriverManager formisce al progrmma il driver corretto da utilzzare
-                //per lo specifico DBMS in uso, inoltre crea la connessione e permette
-                //di usare quindi i suoi metodi
+                // poi si apre la connessione
+                // la classe DriverManager formisce al progrmma il driver corretto da utilzzare
+                // per lo specifico DBMS in uso, inoltre crea la connessione e permette
+                // di usare quindi i suoi metodi
                 connection = DriverManager.getConnection(url, username, password);
                 connected = true;
             }
-        }catch(ClassNotFoundException e){
-            System.out.println("Errore nel Driver MySQL: controlla il percorso"+
-            " alla classe e di aver importato il JAR con la libreria e driver");   
-        }catch(SQLException e){
+        } catch (ClassNotFoundException e) {
+            System.out.println("Errore nel Driver MySQL: controlla il percorso" +
+                    " alla classe e di aver importato il JAR con la libreria e driver");
+        } catch (SQLException e) {
             System.out.println("Errore nell'apertura della connessione nella classe Database " +
-                " controlla che username, password, nome delle schema e url siano corretti"
-            );
+                    " controlla che username, password, nome delle schema e url siano corretti");
         }
     }
 
-
-    //metodo che chiude la connessione
-    public void closeConnection(){
+    // metodo che chiude la connessione
+    public void closeConnection() {
         try {
-            if(connection != null && !connection.isClosed()){
+            if (connection != null && !connection.isClosed()) {
                 connection.close();
                 connected = false;
             }
@@ -83,14 +86,12 @@ public class Database {
         }
     }
 
-    public boolean isConnected(){
+    public boolean isConnected() {
         return connected;
     }
 
-    public Connection getConnection(){
+    public Connection getConnection() {
         return connection;
     }
 
-
 }
-
